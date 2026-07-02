dcaf354233a675e8033167e1993b681f0adfa751
Homer Quan <homerquan@gmail.com>
2026-07-02 14:58:51 -0400

    auto: update all repos

--- lib/mirror_neuron/cluster/manager.ex
+++ lib/mirror_neuron/cluster/manager.ex
@@ -18,6 +18,10 @@ defmodule MirrorNeuron.Cluster.Manager do
               name: to_string(node),
               display_name: node_display_name(node, state, hardware_info),
               hostname: node_hostname(hardware_info),
+              address: Map.get(state, "address") || node_host(node),
+              grpc_host: Map.get(state, "grpc_host") || node_host(node),
+              grpc_port: Map.get(state, "grpc_port") || node_grpc_port(state),
+              native_sdk_grpc: native_sdk_grpc_info(node, state, hardware_info),
               status: Map.get(state, "status", "healthy"),
               scheduling_eligible: Map.get(state, "scheduling_eligible", true),
               drain: Map.get(state, "drain"),
@@ -71,7 +75,7 @@ defmodule MirrorNeuron.Cluster.Manager do
 
   defp fetch_node_info(node) do
     if node == NodeAdapter.self() do
-      {:ok, {LeaseManager.stats(), MirrorNeuron.Cluster.Hardware.info()}}
+      {:ok, {LeaseManager.stats(), local_hardware_info()}}
     else
       case NodeAdapter.rpc_call(node, __MODULE__, :local_info, [], 5_000) do
         {:badrpc, reason} -> {:error, inspect(reason)}
@@ -83,7 +87,12 @@ defmodule MirrorNeuron.Cluster.Manager do
 
   @doc false
   def local_info do
-    {LeaseManager.stats(), MirrorNeuron.Cluster.Hardware.info()}
+    {LeaseManager.stats(), local_hardware_info()}
+  end
+
+  defp local_hardware_info do
+    MirrorNeuron.Cluster.Hardware.info()
+    |> Map.put("native_sdk_grpc", native_sdk_grpc_node_info())
   end
 
   defp runtime_connected_nodes(self_node) do
@@ -112,5 +121,67 @@ defmodule MirrorNeuron.Cluster.Manager do
 
   defp node_hostname(hardware) do
     get_in(hardware, [:platform, :hostname]) || get_in(hardware, ["platform", "hostname"])
+  end
+
+  defp node_host(node) do
+    node
+    |> to_string()
+    |> String.split("@", parts: 2)
+    |> case do
+      [_name, host] -> host
+      _ -> nil
+    end
+  end
+
+  defp node_grpc_port(state) do
+    Map.get(state, "grpc_port") ||
+      advertised_grpc_port()
+  end
+
+  defp native_sdk_grpc_info(node, state, hardware) do
+    Map.get(state, "native_sdk_grpc") ||
+      Map.get(state, :native_sdk_grpc) ||
+      Map.get(hardware, "native_sdk_grpc") ||
+      Map.get(hardware, :native_sdk_grpc) ||
+      if(node == NodeAdapter.self(), do: native_sdk_grpc_node_info(), else: nil)
+  end
+
+  defp native_sdk_grpc_node_info do
+    host =
+      System.get_env("MN_NATIVE_SDK_GRPC_ADVERTISE_HOST") ||
+        System.get_env("MN_NETWORK_ADVERTISE_HOST") ||
+        node_host(NodeAdapter.self())
+
+    port = native_sdk_grpc_port()
+    target = if host in [nil, ""], do: "", else: "#{host}:#{port}"
+
+    %{
+      "enabled" => host not in [nil, ""] and port not in [nil, ""],
+      "host" => host || "",
+      "port" => port,
+      "target" => target,
+      "bind_host" => System.get_env("MN_NATIVE_SDK_GRPC_HOST") || ""
+    }
+  end
+
+  defp native_sdk_grpc_port do
+    System.get_env("MN_NATIVE_SDK_GRPC_ADVERTISE_PORT") ||
+      System.get_env("MN_NATIVE_SDK_GRPC_PORT") ||
+      "55052"
+  end
+
+  defp advertised_grpc_port do
+    case System.get_env("MN_GRPC_ADVERTISE_PORT") do
+      nil -> MirrorNeuron.Config.integer("MN_GRPC_PORT", :grpc_port)
+      "" -> MirrorNeuron.Config.integer("MN_GRPC_PORT", :grpc_port)
+      value -> parse_grpc_port(value)
+    end
+  end
+
+  defp parse_grpc_port(value) do
+    case Integer.parse(value) do
+      {port, ""} -> port
+      _ -> MirrorNeuron.Config.integer("MN_GRPC_PORT", :grpc_port)
+    end
   end
 end
